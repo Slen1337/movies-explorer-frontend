3c4e8adde6fc209ad18176d74c56f24e574973bc
Dmitry Nikonov <saylen211@gmail.com>
2021-03-17 22:47:32 +0300

    Revert "rev check 3"
    This reverts commit 51acc79cf0de4eda8e2d95ca6ef97440ea4f57ef.

--- src/components/Header/HeaderBox/HeaderBox.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function HeaderBox({ title }) {
-  return (
-    <div className="header__box">
-      <h2 className="header__box_title">{title}</h2>
-    </div>
-  );
-}
